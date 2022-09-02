492e9933adf57ce6b20b1d792030e767bc4f09a2
Mayeths <wow@mayeths.com>
2022-09-02 10:25:50 +0800

    Add tass.h

---
+++ include/tass/tass.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef TASS_H_
+#define TASS_H_
+
+#include "vec/VAbstract.hpp"
+#include "vec/VD.hpp"
+#include "vec/VP.hpp"
+
+#include "mat/MAbstract.hpp"
+#include "mat/MP.hpp"
+
+#include "pc/PCAbstract.hpp"
+#include "pc/PCP.hpp"
+#include "pc/PCPH.hpp"
+
+#include "iss/ISSAbstract.hpp"
+#include "iss/CGSolver.hpp"
+#include "iss/PIPECGSolver.hpp"
+
+#endif /*TASS_H_*/
