3a0a72612290e3e85285933b0e6d310328bed135
Mayeths <wow@mayeths.com>
2022-09-20 16:39:03 +0800

    Refactor

--- include/mys/hrtimer.h
+++ include/mys/hrtimer.h
@@ -3,20 +3,20 @@
 #include <stdint.h>
 #include "macro.h"
 
-#if defined(ARCH_AARCH64)
+#if defined(ARCH_AARCH64) /* AArch64 specific timer */
 #define HRNAME "High-resolution timer by AArch64 assembly"
 #define HRTICK_T uint64_t
 #define HRTICK(v) __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(v));
 #define HRFREQ(v) __asm__ __volatile__("mrs %0, CNTFRQ_EL0" : "=r"(v));
 
-#else /* ~1us precision */
+#else /* Default time (~1us precision) */
 #include <time.h>
 #define HRNAME "High-resolution timer by time.h clock_gettime"
 #define HRTICK_T double
-#define HRTICK(v) do { \
-    struct timespec _t; \
-    clock_gettime(CLOCK_REALTIME, &_t); \
-    v = _t.tv_sec + _t.tv_nsec / 1000000000.; \
+#define HRTICK(v) do {                        \
+    struct timespec _t;                       \
+    clock_gettime(CLOCK_REALTIME, &_t);       \
+    v = _t.tv_sec + _t.tv_nsec / (double)1e9; \
 } while (0)
 #define HRFREQ(v) do { v = 1; } while (0)
 #endif
