c7bfbbc91d14ddf2d5b1a8e9376cd1f53a76024f
Mayeths <wow@mayeths.com>
2022-09-26 11:58:18 +0800

    Make hrtime distinguish

--- include/mys/hrtimer.h
+++ include/mys/hrtimer.h
@@ -3,25 +3,65 @@
 #include <stdint.h>
 #include "macro.h"
 
-#if defined(ARCH_AARCH64) /* AArch64 specific timer */
-#define HRNAME "High-resolution timer by AArch64 assembly"
-#define HRTICK_T uint64_t
-#define HRTICK(v) __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(v));
-#define HRFREQ(v) __asm__ __volatile__("mrs %0, CNTFRQ_EL0" : "=r"(v));
+#if defined(ARCH_AARCH64)
+static inline const char *hrname() {
+    return "High-resolution timer by AArch64 assembly (10ns)";
+}
+static inline double hrfreq() {
+    uint64_t f;
+    __asm__ __volatile__("mrs %0, CNTFRQ_EL0" : "=r"(f));
+    return (double)f;
+}
+static inline double hrtime() {
+    uint64_t t;
+    __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(t));
+    return ((double)t) / hrfreq();
+}
 
-#else /* Default time (~1us precision) */
+#elif defined(POSIX_COMPLIANCE)
+/*
+ * clock_gettime [https://man7.org/linux/man-pages/man2/clock_gettime.2.html]
+ * gettimeofday  [https://linux.die.net/man/2/gettimeofday]
+ * It often takes 4 ns and doesn't involve any system call [https://stackoverflow.com/a/42190077]
+ * Difference between CLOCK_REALTIME and CLOCK_MONOTONIC [https://stackoverflow.com/a/3527632]
+ */
 #include <time.h>
-#define HRNAME "High-resolution timer by time.h clock_gettime"
-#define HRTICK_T double
-#define HRTICK(v) do {                        \
-    struct timespec _t;                       \
-    clock_gettime(CLOCK_REALTIME, &_t);       \
-    v = _t.tv_sec + _t.tv_nsec / (double)1e9; \
-} while (0)
-#define HRFREQ(v) do { v = 1; } while (0)
-#endif
+static inline const char *hrname() {
+    return "High-resolution timer by <time.h> (1us~10us)"
+}
+static inline double hrfreq() {
+    return 1;
+}
+static inline double hrtime() {
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + ((double)t.tv_usec) / 1e6;
+    /* It seems like these functions have the same resolution */
+    // struct timespec t;
+    // clock_gettime(CLOCK_REALTIME, &t);
+    // return (double)t.tv_sec + (double)t.tv_nsec / (double)1e9;
+}
 
-static inline const char *hrname() { return HRNAME; }
-static inline double hrfreq() { HRTICK_T f; HRFREQ(f); return ((double)f); }
-static inline double hrtime() { HRTICK_T t; HRTICK(t); return ((double)t) / hrfreq(); }
-static inline double hrdiff(double t0) { return hrtime() - t0; }
+#elif defined(OS_WINDOWS)
+/*
+ * https://stackoverflow.com/a/5801863
+ * https://stackoverflow.com/a/26945754
+ */
+#include <windows.h>
+static inline const char *hrname() {
+    return "High-resolution timer by <windows.h> (1us~10us)"
+}
+static inline double hrfreq() {
+    LARGE_INTEGER f;
+    if (!QueryPerformanceFrequency(&f))
+        return -1;
+    return (double)f.QuadPart;
+}
+static inline double hrtime() {
+    LARGE_INTEGER t;
+    if (!QueryPerformanceCounter(&t))
+        return -1;
+    return ((double)t.QuadPart) / hrfreq();
+}
+
+#endif
--- include/mys/macro.h
+++ include/mys/macro.h
@@ -47,6 +47,10 @@
 #define OS_UNKNOWN
 #endif
 
+#if defined(OS_LINUX) || defined(OS_MACOS) || defined(OS_ANDROID)
+#define POSIX_COMPLIANCE
+#endif
+
 /* Arch Detection */
 #if defined(__x86_64__) || defined(_M_X64)
 #define ARCH_NAME "x64"
@@ -67,5 +71,23 @@
 #define ARCH_NAME "Unknown"
 #define ARCH_UNKNOWN
 #endif
+
+#define REP2(inst) do { inst; inst; } while (0)
+#define REP4(inst) do { REP2(inst); REP2(inst); } while (0)
+#define REP8(inst) do { REP4(inst); REP4(inst); } while (0)
+#define REP16(inst) do { REP8(inst); REP8(inst); } while (0)
+#define REP32(inst) do { REP16(inst); REP16(inst); } while (0)
+#define REP64(inst) do { REP32(inst); REP32(inst); } while (0)
+#define REP128(inst) do { REP64(inst); REP64(inst); } while (0)
+#define REP256(inst) do { REP128(inst); REP128(inst); } while (0)
+#define REP512(inst) do { REP256(inst); REP256(inst); } while (0)
+#define REP1024(inst) do { REP512(inst); REP512(inst); } while (0)
+
+#define REP5(inst) do { inst; inst; inst; inst; inst; } while (0)
+#define REP10(inst) do { REP5(inst); REP5(inst); } while (0)
+#define REP50(inst) do { REP10(inst); REP10(inst); REP10(inst); REP10(inst); REP10(inst); } while (0)
+#define REP100(inst) do { REP50(inst); REP50(inst); } while (0)
+#define REP500(inst) do { REP100(inst); REP100(inst); REP100(inst); REP100(inst); REP100(inst); } while (0)
+#define REP1000(inst) do { REP500(inst); REP500(inst); } while (0)
 
 #endif /*MYS_MACRO_H_*/
