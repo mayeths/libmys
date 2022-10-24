2adb34373099aba16bd8ad10907ff28b06f9e83a
Mayeths <wow@mayeths.com>
2022-10-24 14:03:20 +0800

    Add rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# -ne 3 ]]; then
+    echo "[USAGE] bash $0 host path"
+    echo "        bash $0 kphuanghp ~/project/libmys/"
+    exit 1
+fi
+
+host=$1
+path=$2
+
+rsync --exclude="ugly/" --exclude="lib/" -avz $host:$path .
