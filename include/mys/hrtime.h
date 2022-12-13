f2487b89c4a383a51152fa07a278df41c778e55b
huanghp <wow@mayeths.com>
2022-12-13 10:25:32 +0800

    Add hrtick

--- include/mys/hrtime.h
+++ include/mys/hrtime.h
@@ -11,27 +11,35 @@
 static inline const char *hrname() {
     return "High-resolution timer by AArch64 assembly (10ns)";
 }
-static inline double hrfreq() {
-    uint64_t f;
-    __asm__ __volatile__("mrs %0, CNTFRQ_EL0" : "=r"(f));
-    return (double)f;
-}
-static inline double hrtime() {
+static inline uint64_t hrtick() {
     uint64_t t;
     __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(t));
-    return ((double)t) / hrfreq();
+    return t;
+}
+static inline uint64_t hrfreq() {
+    uint64_t f;
+    __asm__ __volatile__("mrs %0, CNTFRQ_EL0" : "=r"(f));
+    return f;
+}
+static inline double hrtime() {
+    return (double)hrtick() / (double)hrfreq();
 }
 
 #elif defined(ARCH_X64) && defined(TSC_FREQ) && TSC_FREQ > 1
-static inline uint64_t tsc_tick();
 static inline const char *hrname() {
     return "High-resolution timer by X64 assembly (TSC_FREQ=" STR(TSC_FREQ) ")";
 }
-static inline double hrfreq() {
-    return (double)TSC_FREQ;
+static inline uint64_t hrtick() {
+    uint32_t lo, hi;
+    __asm__ __volatile__("mfence":::"memory");
+    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+    return (uint64_t)hi << 32 | (uint64_t)lo;
+}
+static inline uint64_t hrfreq() {
+    return (uint64_t)TSC_FREQ;
 }
 static inline double hrtime() {
-    return (double)tsc_tick() / (double)hrfreq();
+    return (double)hrtick() / (double)hrfreq();
 }
 
 #elif defined(POSIX_COMPLIANCE)
@@ -41,21 +49,26 @@ static inline double hrtime() {
  * It often takes 4 ns and doesn't involve any system call [https://stackoverflow.com/a/42190077]
  * Difference between CLOCK_REALTIME and CLOCK_MONOTONIC [https://stackoverflow.com/a/3527632]
  */
+#include <stdlib.h>
 #include <sys/time.h>
 static inline const char *hrname() {
     return "High-resolution timer by <time.h> (1us~10us)";
 }
-static inline double hrfreq() {
-    return 1;
-}
-static inline double hrtime() {
+static inline uint64_t hrtick() {
     struct timeval t;
     gettimeofday(&t, NULL);
-    return (double)t.tv_sec + ((double)t.tv_usec) / 1e6;
+    return (uint64_t)t.tv_sec * 1000 * 1000 + (uint64_t)t.tv_usec;
     /* It seems like these functions have the same resolution */
     // struct timespec t;
     // clock_gettime(CLOCK_REALTIME, &t);
-    // return (double)t.tv_sec + (double)t.tv_nsec / (double)1e9;
+    // return (uint64_t)t.tv_sec * 1000 * 1000 * 1000 + (uint64_t)t.tv_nsec;
+}
+static inline double hrfreq() {
+    return 1000 * 1000;
+    // return 1000 * 1000 * 1000;
+}
+static inline double hrtime() {
+    return (double)hrtick() / (double)hrfreq();
 }
 
 #elif defined(OS_WINDOWS)
@@ -67,40 +80,40 @@ static inline double hrtime() {
 static inline const char *hrname() {
     return "High-resolution timer by <windows.h> (1us~10us)";
 }
-static inline double hrfreq() {
-    LARGE_INTEGER f;
-    if (!QueryPerformanceFrequency(&f))
-        return -1;
-    return (double)f.QuadPart;
-}
-static inline double hrtime() {
+static inline uint64_t hrtick() {
     LARGE_INTEGER t;
     if (!QueryPerformanceCounter(&t))
-        return -1;
-    return ((double)t.QuadPart) / hrfreq();
+        return 0;
+    return (uint64_t)t.QuadPart;
+}
+static inline uint64_t hrfreq() {
+    LARGE_INTEGER f;
+    if (!QueryPerformanceFrequency(&f))
+        return 0;
+    return (uint64_t)f.QuadPart;
+}
+static inline double hrtime() {
+    return (double)hrtick() / (double)hrfreq();
 }
 
 #endif
 
 /******* AUX *******/
 
-#if defined(ARCH_X64)
-static inline uint64_t tsc_tick()
-{
+#if defined(ARCH_X64) && defined(POSIX_COMPLIANCE)
+#include <stdlib.h>
+#include <unistd.h>
+static inline uint64_t tsctick() {
     uint32_t lo, hi;
     __asm__ __volatile__("mfence":::"memory");
     __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
-    return (uint64_t)hi << 32 | lo;
+    return (uint64_t)hi << 32 | (uint64_t)lo;
 }
-#if defined(POSIX_COMPLIANCE)
-#include <stdlib.h>
-#include <sys/time.h>
 static inline uint64_t test_tsc_freq()
 {
-    uint64_t raw1 = tsc_tick();
+    uint64_t raw1 = tsctick();
     sleep(1);
-    uint64_t raw2 = tsc_tick();
+    uint64_t raw2 = tsctick();
     return raw2 - raw1;
 }
-#endif
 #endif
