4f91d503a1532457295b33afbd73068f15455ff8
Mayeths <wow@mayeths.com>
2023-01-15 20:38:56 +0800

    Add cudaCalloc and cudaDuplicate

--- include/mys/cuda.cuh
+++ include/mys/cuda.cuh
@@ -9,3 +9,24 @@
         #fncall, (int)cudaSuccess, (int)val,  \
         cudaGetErrorString(val));             \
 } while (0)
+
+template<typename T>
+static inline cudaError_t cudaCalloc(T **ptr, size_t nmemb, size_t size) { 
+    cudaError_t err = cudaSuccess;
+    err = cudaMalloc((void **)ptr, nmemb * size);
+    if (err != cudaSuccess) return err;
+    err = cudaMemset((void *)*ptr, 0, nmemb * size);
+    return err;
+}
+
+template<typename T>
+void cudaDuplicate(T **dst, T *src, size_t nmemb, size_t size, cudaMemcpyKind kind)
+{
+    const size_t nbytes = nmemb * size;
+    if (kind == cudaMemcpyHostToHost || kind == cudaMemcpyDeviceToHost) {
+        CHKPTR(*dst = (T *)malloc(nbytes));
+    } else if (kind == cudaMemcpyHostToDevice || kind == cudaMemcpyDeviceToDevice) {
+        CHKCUDA(cudaMalloc((void **)dst, nbytes));
+    }
+    CHKCUDA(cudaMemcpy((void *)*dst, src, nbytes, kind));
+}
