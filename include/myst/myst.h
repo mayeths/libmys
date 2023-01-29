d03748f101622f6d1e4dd5d42994bf41b3880e8c
mayeths <wow@mayeths.com>
2023-01-30 00:16:53 +0800

    Use proxy myst_ for gptl_

--- include/myst/myst.h
+++ include/myst/myst.h
@@ -1,2 +1,110 @@
 #pragma once
-#include "./gptl-customized/gptl.h"
+// #include "./gptl-customized/gptl.h"
+
+/*
+** Options settable by a call to MYSTsetoption() (default in parens)
+** These numbers need to be small integers because MYSTsetoption can
+** be passed PAPI counters, and we need to avoid collisions in that
+** integer space. PAPI presets are big negative integers, and PAPI
+** native events are big positive integers.
+*/
+
+typedef enum {
+  MYSTsync_mpi        = 0,  // Synchronize before certain MPI calls (PMPI-mode only)
+  MYSTwall            = 1,  // Collect wallclock stats (true)
+  MYSTcpu             = 2,  // Collect CPU stats (false)*/
+  MYSTabort_on_error  = 3,  // Abort on failure (false)
+  MYSToverhead        = 4,  // Estimate overhead of underlying timing routine (true)
+  MYSTdepthlimit      = 5,  // Only print timers this depth or less in the tree (inf)
+  MYSTverbose         = 6,  // Verbose output (false)
+  MYSTnarrowprint     = 7,  // Print PAPI and derived stats in 8 columns not 16 (true)
+  MYSTpercent         = 9,  // Add a column for percent of first timer (false)
+  MYSTpersec          = 10, // Add a PAPI column that prints "per second" stats (true)
+  MYSTmultiplex       = 11, // Allow PAPI multiplexing (false)
+  MYSTdopr_preamble   = 12, // Print preamble info (true)
+  MYSTdopr_threadsort = 13, // Print sorted thread stats (true)
+  MYSTdopr_multparent = 14, // Print multiple parent info (true)
+  MYSTdopr_collision  = 15, // Print hastable collision info (true)
+  MYSTdopr_memusage   = 27, // Print memory usage stats when growth exceeds some threshhold %
+  MYSTprint_method    = 16, // Tree print method: first parent, last parent,
+			    //   most frequent, or full tree (most frequent)
+  MYSTtablesize       = 50, // per-thread size of hash table
+  MYSTmaxthreads      = 51, // maximum number of threads
+  MYSTonlyprint_rank0 = 52, // Restrict printout to rank 0 when MPI enabled
+  MYSTmem_growth      = 53, // Print info when mem usage (RSS) has grown by more than some percent
+
+  // These are derived counters based on PAPI counters. All default to false
+  MYST_IPC           = 17, // Instructions per cycle
+  MYST_LSTPI         = 21, // Load-store instruction fraction
+  MYST_DCMRT         = 22, // L1 miss rate (fraction)
+  MYST_LSTPDCM       = 23, // Load-stores per L1 miss
+  MYST_L2MRT         = 24, // L2 miss rate (fraction)
+  MYST_LSTPL2M       = 25, // Load-stores per L2 miss
+  MYST_L3MRT         = 26  // L3 read miss rate (fraction)
+} MYSToption;
+
+/*
+** Underlying wallclock timer: optimize for best granularity with least overhead.
+** These numbers need not be distinct from the above because these are passed
+** to MYSTsetutr() and the above are passed to MYSTsetoption()
+*/
+typedef enum {
+  MYSTgettimeofday   = 1, // ubiquitous but slow
+  MYSTnanotime       = 2, // only available on x86
+  MYSTmpiwtime       = 4, // MPI_Wtime
+  MYSTclockgettime   = 5, // clock_gettime
+  MYSTplacebo        = 7, // do-nothing
+  MYSTread_real_time = 3  // AIX only
+} MYSTFuncoption;
+
+// How to report parent/child relationships at print time (for children with multiple parents)
+typedef enum {
+  MYSTfirst_parent  = 1,  // first parent found
+  MYSTlast_parent   = 2,  // last parent found
+  MYSTmost_frequent = 3,  // most frequent parent (default)
+  MYSTfull_tree     = 4   // complete call tree
+} MYSTMethod;
+
+// User-callable function prototypes: all require C linkage
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int MYSTsetoption (const int, const int);
+extern int MYSTinitialize (void);
+extern int MYSTstart (const char *);
+extern int MYSTinit_handle (const char *, int *);
+extern int MYSTstart_handle (const char *, int *);
+extern int MYSTstop (const char *);
+extern int MYSTstop_handle (const char *, int *);
+extern int MYSTstamp (double *, double *, double *);
+extern int MYSTpr (const int);
+extern int MYSTpr_file (const char *);
+extern int MYSTreset (void);
+extern int MYSTreset_timer (const char *);
+extern int MYSTfinalize (void);
+extern int MYSTget_memusage (float *);
+extern int MYSTprint_memusage (const char *);
+extern int MYSTprint_rusage (const char *);
+extern int MYSTget_procsiz (float *, float *);
+extern int MYSTenable (void);
+extern int MYSTdisable (void);
+extern int MYSTsetutr (const int);
+extern int MYSTquery (const char *, int, int *, int *, double *, double *, double *,
+		      long long *, const int);
+extern int MYSTget_wallclock (const char *, int, double *);
+extern int MYSTget_wallclock_latest (const char *, int, double *);
+extern int MYSTget_threadwork (const char *, double *, double *);
+extern int MYSTstartstop_val (const char *, double);
+extern int MYSTget_nregions (int, int *);
+extern int MYSTget_regionname (int, int, char *, int);
+extern int MYST_PAPIlibraryinit (void);
+extern int MYSTevent_name_to_code (const char *, int *);
+extern int MYSTevent_code_to_name (const int, char *);
+extern int MYSTget_eventvalue (const char *, const char *, int, double *);
+extern int MYSTnum_errors (void);
+extern int MYSTnum_warn (void);
+extern int MYSTget_count (const char *, int, int *);
+#ifdef __cplusplus
+}
+#endif
--- include/myst/myst.impl.h
+++ include/myst/myst.impl.h
@@ -1,2 +1,127 @@
 #pragma once
 #include "./gptl-customized/gptl.c"
+
+int MYSTsetoption (const int option, const int val)
+{
+    return GPTLsetoption(option, val);
+}
+
+int MYSTinitialize (void)
+{
+    return GPTLinitialize();
+}
+
+int MYSTstart (const char *name)
+{
+    return GPTLstart(name);
+}
+
+int MYSTinit_handle (const char *name, int *handle)
+{
+    return GPTLinit_handle(name, handle);
+}
+
+int MYSTstart_handle (const char *name, int *handle)
+{
+    return GPTLstart_handle(name, handle);
+}
+
+int MYSTstop (const char *name)
+{
+    return GPTLstop(name);
+}
+
+int MYSTstop_handle (const char *name, int *handle)
+{
+    return GPTLstop_handle(name, handle);
+}
+
+int MYSTstamp (double *wall, double *usr, double *sys)
+{
+    return GPTLstamp(wall, usr, sys);
+}
+
+int MYSTpr (const int id)
+{
+    return GPTLpr(id);
+}
+
+int MYSTpr_file (const char *outfile)
+{
+    return GPTLpr_file(outfile);
+}
+
+int MYSTreset (void)
+{
+    return GPTLreset();
+}
+
+int MYSTreset_timer (const char *name)
+{
+    return GPTLreset_timer(name);
+}
+
+int MYSTfinalize (void)
+{
+    return GPTLfinalize();
+}
+
+int MYSTenable (void)
+{
+    return GPTLenable();
+}
+
+int MYSTdisable (void)
+{
+    return GPTLdisable();
+}
+
+int MYSTsetutr (const int option)
+{
+    return GPTLsetutr(option);
+}
+
+int MYSTquery (const char *name, int t, int *count, int *onflg, double *wallclock, double *dusr, double *dsys, long long *papicounters_out, const int maxcounters)
+{
+    return GPTLquery(name, t, count, onflg, wallclock, dusr, dsys, papicounters_out, maxcounters);
+}
+
+int MYSTget_wallclock (const char *timername, int t, double *value)
+{
+    return GPTLget_wallclock(timername, t, value);
+}
+
+int MYSTget_wallclock_latest (const char *timername, int t, double *value)
+{
+    return GPTLget_wallclock_latest(timername, t, value);
+}
+
+int MYSTget_threadwork (const char *name, double *maxwork, double *imbal)
+{
+    return GPTLget_threadwork(name, maxwork, imbal);
+}
+
+int MYSTstartstop_val (const char *name, double value)
+{
+    return GPTLstartstop_val(name, value);
+}
+
+int MYSTget_nregions (int t, int *nregions)
+{
+    return GPTLget_nregions(t, nregions);
+}
+
+int MYSTget_regionname (int t, int region, char *name, int nc)
+{
+    return GPTLget_regionname(t, region, name, nc);
+}
+
+int MYSTget_eventvalue (const char *timername, const char *eventname, int t, double *value)
+{
+    return GPTLget_eventvalue(timername, eventname, t, value);
+}
+
+int MYSTget_count (const char *timername, int t, int *count)
+{
+    return GPTLget_count(timername, t, count);
+}
