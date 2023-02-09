acb682f62d8fa82a67b85f02dd923fcbd878114f
mayeths <wow@mayeths.com>
2023-02-09 22:43:27 +0800

    Refactor random.h

--- include/mys/impl.h
+++ include/mys/impl.h
@@ -21,9 +21,167 @@
 #include "random.h"
 
 mys_thread_local int mys_errno = 0;
-mys_thread_local uint64_t __legacy_x = __UINT64_INVALID;
-mys_thread_local uint64_t __splitmix64_x = __UINT64_INVALID;
-mys_thread_local uint64_t __xoroshiro128_x[2] = {__UINT64_INVALID, __UINT64_INVALID};
+
+mys_thread_local _mys_rand_G_t _mys_rand_G = {
+    .inited = false,
+    .splitmix64_x = 0,
+    .xoroshiro128_x = {0, 0},
+};
+
+MYS_API void mys_rand_init()
+{
+    if (_mys_rand_G.inited == true)
+        return;
+    uint64_t seed[2];
+    seed[0] = mys_rand_seed();
+    seed[1] = MYS_UINT64_MAX - seed[0];
+    mys_rand_srand(seed[0], seed[1]);
+    _mys_rand_G.inited = true;
+}
+
+MYS_API void mys_rand_srand(uint64_t a0, uint64_t a1)
+{
+    srand((unsigned int)a0); /* mys_rand_legacy */
+    _mys_rand_G.splitmix64_x = a0;
+    _mys_rand_G.xoroshiro128_x[0] = a0;
+    _mys_rand_G.xoroshiro128_x[1] = a1;
+}
+
+MYS_API uint64_t mys_rand_seed()
+{
+#if defined(MYS_FAKE_RANDOM)
+    return mys_rand_seed_fixed();
+#elif defined(ARCH_X64)
+    return mys_rand_seed_x64();
+#elif defined(ARCH_AARCH64)
+    return mys_rand_seed_aarch64();
+#else
+    return mys_rand_seed_time();
+#endif
+}
+
+MYS_API uint64_t mys_rand_seed_fixed()
+{
+    return (uint64_t)0x5A5A5555AAAA55AA;
+}
+
+MYS_API uint64_t mys_rand_seed_time()
+{
+    uint64_t t = (uint64_t)time(NULL);
+    return (t << 32) | (t & 0xAAAA5555);
+}
+
+#ifdef ARCH_X64
+MYS_API uint64_t mys_rand_seed_x64()
+{
+    uint32_t lo, hi;
+    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
+    uint64_t t = ((uint64_t)hi << 32) | (uint64_t)lo;
+    return (t << 32) | (t & 0xAAAA5555); /* A(1010)5(0101) won't INVALID(1111_1111) again */
+}
+#endif
+
+#ifdef ARCH_AARCH64
+MYS_API uint64_t mys_rand_seed_aarch64()
+{
+    uint64_t t;
+    __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(t));
+    return (t << 32) | (t & 0xAAAA5555);
+}
+#endif
+
+MYS_API uint64_t mys_rand_legacy_u64()
+{
+    mys_rand_init();
+    double perc = (double)rand() / (double)RAND_MAX;
+    return perc * (double)MYS_UINT64_MAX;
+}
+
+MYS_API uint64_t mys_rand_splitmix_u64()
+{
+    mys_rand_init();
+    uint64_t z = (_mys_rand_G.splitmix64_x += 0x9e3779b97f4a7c15);
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+    return z ^ (z >> 31);
+}
+
+static inline uint64_t __rotl(const uint64_t x, int k)
+{
+    return (x << k) | (x >> (64 - k));
+}
+MYS_API uint64_t mys_rand_xoroshiro128ss_u64()
+{
+    mys_rand_init();
+    const uint64_t s0 = _mys_rand_G.xoroshiro128_x[0];
+    uint64_t s1 = _mys_rand_G.xoroshiro128_x[1];
+    const uint64_t result = __rotl(s0 * 5, 7) * 9;
+    s1 ^= s0;
+    _mys_rand_G.xoroshiro128_x[0] = __rotl(s0, 24) ^ s1 ^ (s1 << 16);
+    _mys_rand_G.xoroshiro128_x[1] = __rotl(s1, 37);
+    return result;
+}
+
+MYS_API uint64_t mys_randu64(uint64_t minimum, uint64_t maximum)
+{
+#if defined(MYS_LEGACY_RANDOM)
+    uint64_t rand = mys_rand_legacy_u64();
+#elif defined(MYS_SPLITMIX_RANDOM)
+    uint64_t rand = mys_rand_splitmix_u64();
+#else
+    uint64_t rand = mys_rand_xoroshiro128ss_u64();
+#endif
+    if (maximum <= minimum)
+        return minimum;
+    if (minimum == 0 && maximum == MYS_UINT64_MAX)
+        return rand;
+    double range = maximum - minimum + 1;
+    double perc = (double)rand / (double)MYS_UINT64_MAX;
+    return perc * range;
+}
+
+MYS_API int64_t mys_randi64(int64_t minimum, int64_t maximum)
+{
+    uint64_t _min = (uint64_t)minimum;
+    uint64_t _max = (uint64_t)maximum;
+    uint64_t result = mys_randu64(_min, _max);
+    return *(int64_t *)((void *)&result);
+}
+
+MYS_API double mys_randf64(double minimum, double maximum)
+{
+    double range = maximum - minimum;
+    double div = (double)MYS_UINT64_MAX / range;
+    return minimum + (mys_randu64(0, MYS_UINT64_MAX) / div);
+}
+
+MYS_API uint32_t mys_randu32(uint32_t minimum, uint32_t maximum)
+{
+    return (uint32_t)mys_randu64(minimum, maximum);
+}
+
+MYS_API int32_t mys_randi32(int32_t minimum, int32_t maximum)
+{
+    return (int32_t)mys_randi64(minimum, maximum);
+}
+
+MYS_API float mys_randf32(float minimum, float maximum)
+{
+    return (float)mys_randf64(minimum, maximum);
+}
+
+MYS_API const char *mys_randname()
+{
+#if defined(MYS_LEGACY_RANDOM)
+    return "legacy random generator";
+#elif defined(MYS_SPLITMIX_RANDOM)
+    return "splitmix random generator";
+#else
+    return "xoroshiro128ss random generator";
+#endif
+}
+
+
 mys_log_G_t mys_log_G = {
     .level = MYS_LOG_TRACE,
     .last_level = MYS_LOG_TRACE,
--- include/mys/random.h
+++ include/mys/random.h
@@ -9,56 +9,66 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-#define __UINT64_MAX 0xFFFFFFFFFFFFFFFFULL
-#define __UINT64_INVALID __UINT64_MAX
-
-#if defined(MYS_FAKE_RANDOM)
-static inline uint64_t __seed()
-{
-    return (uint64_t)0x5A5A5555AAAA55AA;
-}
-#elif defined(ARCH_X64)
-static inline uint64_t __seed()
-{
-    uint32_t lo, hi;
-    __asm__ __volatile__("rdtsc" : "=a" (lo), "=d" (hi));
-    uint64_t t = ((uint64_t)hi << 32) | (uint64_t)lo;
-    return (t << 32) | (t & 0xAAAA5555); /* A(1010)5(0101) won't INVALID(1111_1111) again */
-}
-#elif defined(ARCH_AARCH64)
-static inline uint64_t __seed()
-{
-    uint64_t t;
-    __asm__ __volatile__("mrs %0, CNTVCT_EL0" : "=r"(t));
-    return (t << 32) | (t & 0xAAAA5555);
-}
-#else
-static inline uint64_t __seed()
-{
-    uint64_t t = (uint64_t)time(NULL);
-    return (t << 32) | (t & 0xAAAA5555);
-}
-#endif
+#include <stdbool.h>
 
 #include "thread.h"
 
+#define MYS_UINT64_MAX 0xFFFFFFFFFFFFFFFFULL
+
+/**
+ * @brief Generate random uint64_t value
+ * 
+ * @param minimum minimum value (inclusive)
+ * @param maximum minimum value (inclusive)
+ * @return uint64_t random value between [minimum, maximum]
+ */
+MYS_API uint64_t mys_randu64(uint64_t minimum, uint64_t maximum);
+MYS_API int64_t mys_randi64(int64_t minimum, int64_t maximum);
+MYS_API double mys_randf64(double minimum, double maximum);
+MYS_API uint32_t mys_randu32(uint32_t minimum, uint32_t maximum);
+MYS_API int32_t mys_randi32(int32_t minimum, int32_t maximum);
+MYS_API float mys_randf32(float minimum, float maximum);
+MYS_API const char *mys_randname();
+
+////// Legacy
+
+static inline uint64_t randu64(uint64_t minimum, uint64_t maximum) {
+    return mys_randu64(minimum, maximum);
+}
+static inline int64_t randi64(int64_t minimum, int64_t maximum) {
+    return mys_randi64(minimum, maximum);
+}
+static inline double randf64(double minimum, double maximum) {
+    return mys_randf64(minimum, maximum);
+}
+static inline uint32_t randu32(uint32_t minimum, uint32_t maximum) {
+    return mys_randu32(minimum, maximum);
+}
+static inline int32_t randi32(int32_t minimum, int32_t maximum) {
+    return mys_randi32(minimum, maximum);
+}
+static inline float randf32(float minimum, float maximum) {
+    return mys_randf32(minimum, maximum);
+}
+static inline const char *randname() {
+    return mys_randname();
+}
+
+////// Advanced
+
+MYS_API void mys_rand_init();
+MYS_API void mys_rand_srand(uint64_t a0, uint64_t a1);
+MYS_API uint64_t mys_rand_seed();
+MYS_API uint64_t mys_rand_seed_fixed();
+MYS_API uint64_t mys_rand_seed_time();
+#ifdef ARCH_X64
+MYS_API uint64_t mys_rand_seed_x64();
+#endif
+#ifdef ARCH_AARCH64
+MYS_API uint64_t mys_rand_seed_aarch64();
+#endif
 /* legacy random */
-extern mys_thread_local uint64_t __legacy_x;
-static inline void __legacy_init(uint64_t seed)
-{
-    __legacy_x = seed;
-    srand((unsigned int)__legacy_x);
-}
-static inline uint64_t __legacy_rand()
-{
-    if (__legacy_x == __UINT64_INVALID)
-        __legacy_init(__seed());
-    double perc = (double)rand() / (double)RAND_MAX;
-    return perc * (double)__UINT64_MAX;
-}
-
-
+MYS_API uint64_t mys_rand_legacy_u64();
 /* https://prng.di.unimi.it
  * https://prng.di.unimi.it/splitmix64.c
  * This is a fixed-increment version of Java 8's SplittableRandom generator
@@ -67,22 +77,7 @@ static inline uint64_t __legacy_rand()
  * It is a very fast generator passing BigCrush, and it can be useful if
  * for some reason you absolutely want 64 bits of state.
  */
-extern mys_thread_local uint64_t __splitmix64_x; /* The state can be seeded with any value. */
-static inline void __splitmix_init(uint64_t seed)
-{
-    __splitmix64_x = seed;
-}
-static inline uint64_t __splitmix_rand()
-{
-    if (__splitmix64_x == __UINT64_INVALID)
-        __splitmix_init(__seed());
-    uint64_t z = (__splitmix64_x += 0x9e3779b97f4a7c15);
-    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-    return z ^ (z >> 31);
-}
-
-
+MYS_API uint64_t mys_rand_splitmix_u64();
 /* https://prng.di.unimi.it
  * https://prng.di.unimi.it/xoroshiro128starstar.c
  * This is xoroshiro128** 1.0, one of our all-purpose, rock-solid,
@@ -95,95 +90,17 @@ static inline uint64_t __splitmix_rand()
  * a 64-bit seed, we suggest to seed a splitmix64 generator and use its
  * output to fill __xoroshiro128_x.
  */
-extern mys_thread_local uint64_t __xoroshiro128_x[2];
-static inline void __xoroshiro128ss_init(uint64_t seed)
-{
-    __xoroshiro128_x[0] = seed;
-    __xoroshiro128_x[1] = __UINT64_MAX - seed;
-}
-static inline uint64_t __rotl(const uint64_t x, int k)
-{
-    return (x << k) | (x >> (64 - k));
-}
-static inline uint64_t __xoroshiro128ss_rand()
-{
-    if (__xoroshiro128_x[0] == __UINT64_INVALID)
-        __xoroshiro128ss_init(__seed()); /* Suggest to use __splitmix_rand() */
-    const uint64_t s0 = __xoroshiro128_x[0];
-    uint64_t s1 = __xoroshiro128_x[1];
-    const uint64_t result = __rotl(s0 * 5, 7) * 9;
-    s1 ^= s0;
-    __xoroshiro128_x[0] = __rotl(s0, 24) ^ s1 ^ (s1 << 16);
-    __xoroshiro128_x[1] = __rotl(s1, 37);
-    return result;
-}
+MYS_API uint64_t mys_rand_xoroshiro128ss_u64();
 
+////// Internal
 
-static inline const char *randname()
-{
-#if defined(MYS_LEGACY_RANDOM)
-    return "legacy random generator";
-#elif defined(MYS_SPLITMIX_RANDOM)
-    return "splitmix random generator";
-#else
-    return "xoroshiro128ss random generator";
-#endif
-}
+typedef struct _mys_rand_G_t {
+    bool inited;
+    uint64_t splitmix64_x;
+    uint64_t xoroshiro128_x[2];
+} _mys_rand_G_t;
 
-/**
- * @brief Generate random uint64_t value
- * 
- * @param minimum minimum value (inclusive)
- * @param maximum minimum value (inclusive)
- * @return uint64_t random value between [minimum, maximum]
- */
-static inline uint64_t randu64(uint64_t minimum, uint64_t maximum)
-{
-#if defined(MYS_LEGACY_RANDOM)
-    uint64_t rand = __legacy_rand();
-#elif defined(MYS_SPLITMIX_RANDOM)
-    uint64_t rand = __splitmix_rand();
-#else
-    uint64_t rand = __xoroshiro128ss_rand();
-#endif
-    if (maximum <= minimum)
-        return minimum;
-    if (minimum == 0 && maximum == __UINT64_MAX)
-        return rand;
-    double range = maximum - minimum + 1;
-    double perc = (double)rand / (double)__UINT64_MAX;
-    return perc * range;
-}
-
-static inline int64_t randi64(int64_t minimum, int64_t maximum)
-{
-    uint64_t _min = (uint64_t)minimum;
-    uint64_t _max = (uint64_t)maximum;
-    uint64_t result = randu64(_min, _max);
-    return *(int64_t *)((void *)&result);
-}
-
-static inline double randf64(double minimum, double maximum)
-{
-    double range = maximum - minimum;
-    double div = (double)__UINT64_MAX / range;
-    return minimum + (randu64(0, __UINT64_MAX) / div);
-}
-
-static inline uint32_t randu32(uint32_t minimum, uint32_t maximum)
-{
-    return (uint32_t)randu64(minimum, maximum);
-}
-
-static inline int32_t randi32(int32_t minimum, int32_t maximum)
-{
-    return (int32_t)randi64(minimum, maximum);
-}
-
-static inline float randf32(float minimum, float maximum)
-{
-    return (float)randf64(minimum, maximum);
-}
+extern mys_thread_local _mys_rand_G_t _mys_rand_G;
 
 
 /* Tester:
@@ -234,7 +151,7 @@ int main(int argc , char **argv) {
     }
 
     for (int i = 0; i < ssize; i++) {
-        printf("[%03d,%03d) %.3f %d\n", i-minimum, i+1-minimum, ((double)box[i]/n) * 100, box[i]);
+        printf("[%03d,%03d) %.3f %d\n", i-minimum, i+1-minimum, ((double)box[i]/n) * 100, (int)box[i]);
     }
 
     free(buffer);
