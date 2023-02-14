3136eb3b2eb24e823564d6a2de45132aca54dfdd
mayeths <wow@mayeths.com>
2023-02-14 13:42:41 +0800

    Add tmux/

--- script/shrc.sh
+++ script/shrc.sh
@@ -1,7 +1,19 @@
 ###
 
 export MYS_DIR=$(dirname $(dirname "$0")/../)
+if $SHELL --version | grep -q bash; then
+    source bashrc.sh
+elif $SHELL --version | grep -q zsh; then
+    source zshrc.sh
+fi
+
+command -v realpath >/dev/null 2>&1 || realpath() {
+    echo "$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$1")"
+}
+
 export PYTHONDONTWRITEBYTECODE=1
+export MYS_TMUX_CONF=$(realpath $MYS_DIR/tmux/tmux.conf)
+export MYS_TMUX_CONF_LOCAL=$(realpath $MYS_DIR/tmux/tmux.conf.local)
 
 if test $[HISTSIZE] -lt 50000; then
     export HISTSIZE=50000
@@ -9,12 +21,6 @@ fi
 
 if test $[HISTFILESIZE] -lt 50000; then
     export HISTFILESIZE=50000
-fi
-
-if $SHELL --version | grep -q bash; then
-    source bashrc.sh
-elif $SHELL --version | grep -q zsh; then
-    source zshrc.sh
 fi
 
 env_prepend PATH "$MYS_DIR/bin"
