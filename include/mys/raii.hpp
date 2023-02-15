0dd5e7e82f4871028b422c3b7a6ea52892b03ca6
mayeths <wow@mayeths.com>
2023-02-15 22:09:03 +0800

    Remove unused comment

--- include/mys/raii.hpp
+++ include/mys/raii.hpp
@@ -1,12 +1,7 @@
 #pragma once
 
 // https://codereview.stackexchange.com/a/247491
-// https://codereview.stackexchange.com/a/248060 std::array的OpenGL Buffer
-
-// 只关注一个场景：有且只有一个handler的资源，比如PETSC的Mat，Vec，HPSS的Handler，OpenGL的各类Buffer（经典例子）等。
-// 只做一件事：自动Destructor，不拦截其他操作，用get()直接暴露，你可以拿去直接和库交互。所以我们只管Constructor和Destructor
-// 也就是说，我依赖一个库，这个库有资源acquire/destroy操作。我只想自动destroy，其他事情不要想太多（比如都想wrap起来），这是过度设计。
-// 全小写名字，这么短的单词驼峰太奇怪了
+// https://codereview.stackexchange.com/a/248060
 
 #include <string>
 #include <stdexcept>
