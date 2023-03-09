a82c06ecbdf367533f6f85fc30bab19254b167ea
mayeths <wow@mayeths.com>
2023-03-09 16:08:40 +0800

    Fix SHELL detecting

--- script/sh-functions.sh
+++ script/sh-functions.sh
@@ -30,3 +30,42 @@ mys_load_script() {
 mys_realpath() {
     echo "$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$1")"
 }
+
+# Borrow from lmod/init/profile
+mys_find_exec () {
+  local Nm=$1
+  local confPath=$2
+  local execNm=$3
+  eval $Nm=$confPath
+  if [ ! -x $confPath ]; then
+    if [ -x /bin/$execNm ]; then
+       eval $Nm=/bin/$execNm
+    elif [ -x /usr/bin/$execNm ]; then
+       eval $Nm=/usr/bin/$execNm
+    fi
+  fi
+}
+
+# Borrow from lmod/init/profile
+mys_find_shell() {
+    local READLINK_CMD PS_CMD EXPR_CMD BASENAME_CMD my_shell
+    mys_find_exec READLINK_CMD /usr/bin/readlink  readlink
+    mys_find_exec PS_CMD       /bin/ps        ps
+    mys_find_exec EXPR_CMD     /bin/expr      expr
+    mys_find_exec BASENAME_CMD /usr/bin/basename  basename
+
+    if [ -f /proc/$$/exe ]; then
+        my_shell=$($READLINK_CMD /proc/$$/exe)
+    else
+        my_shell=$($PS_CMD -p $$ -ocomm=)
+    fi
+    my_shell=$($EXPR_CMD    "$my_shell" : '-*\(.*\)')
+    my_shell=$($BASENAME_CMD $my_shell)
+
+    case ${my_shell} in
+        bash|zsh|sh) ;;
+        ksh*) my_shell="ksh";;
+        *) my_shell="sh";;
+    esac
+    echo $my_shell
+}
