d2bb931c557c8e0b763e4eb8974ba88506a7fe4c
mayeths <wow@mayeths.com>
2023-03-09 16:33:27 +0800

    Fix alias tmux

--- script/sh-functions.sh
+++ script/sh-functions.sh
@@ -1,36 +1,4 @@
 
-mys_backup_command() {
-    local s
-    $SHELL --version | grep -q bash && s=bash
-    $SHELL --version | grep -q zsh && s=zsh
-    local t
-    [[ $s == "bash" ]] && t=$(type -t $1)
-    [[ $s == "zsh"  ]] && t=$(whence -w $1 | awk '{ print $NF }')
-    if [[ $t == "function" ]]; then
-        eval "$2() $(declare -f $1 | sed 1d)"
-    elif [[ $t == "builtin" || $t == "file" || $t == "command" ]]; then
-        eval "$2() { command $1 \"\$@\" }"
-    elif [[ $t == "alias" ]]; then
-        local p
-        [[ "$s" == "bash" ]] && p="$2"
-        [[ "$s" == "zsh" ]] && p="alias $2"
-        eval $(alias $1 | awk "NR==1,/$1/{sub(/$1/, \"$p\")} 1")
-    else # "keyword" or "reserved"
-        return 1
-    fi
-}
-
-mys_load_script() {
-    local path
-    for path in "$@"; do
-        [[ -f "$path" ]] && source $path
-    done
-}
-
-mys_realpath() {
-    echo "$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$1")"
-}
-
 # Borrow from lmod/init/profile
 mys_find_exec () {
   local Nm=$1
@@ -68,4 +36,34 @@ mys_find_shell() {
         *) my_shell="sh";;
     esac
     echo $my_shell
+}
+
+mys_backup_command() {
+    local s=$(mys_find_shell)
+    local t
+    [[ $s == "bash" ]] && t=$(type -t $1)
+    [[ $s == "zsh"  ]] && t=$(whence -w $1 | awk '{ print $NF }')
+    if [[ $t == "function" ]]; then
+        eval "$2() $(declare -f $1 | sed 1d)"
+    elif [[ $t == "builtin" || $t == "file" || $t == "command" ]]; then
+        eval "$2() { command $1 \"\$@\" }"
+    elif [[ $t == "alias" ]]; then
+        local p
+        [[ "$s" == "bash" ]] && p="$2"
+        [[ "$s" == "zsh" ]] && p="alias $2"
+        eval $(alias $1 | awk "NR==1,/$1/{sub(/$1/, \"$p\")} 1")
+    else # "keyword" or "reserved"
+        return 1
+    fi
+}
+
+mys_load_script() {
+    local path
+    for path in "$@"; do
+        [[ -f "$path" ]] && source $path
+    done
+}
+
+mys_realpath() {
+    echo "$(cd "$(dirname -- "$1")" >/dev/null; pwd -P)/$(basename -- "$1")"
 }
