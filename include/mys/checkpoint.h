cf2e98848c197ce25399193cdc052fc63e812ff4
mayeths <wow@mayeths.com>
2023-05-05 20:53:05 +0800

    Add checkpoint example

--- include/mys/checkpoint.h
+++ include/mys/checkpoint.h
@@ -19,3 +19,19 @@ MYS_API void mys_checkpoint(const char *name_format, ...);
 MYS_API int mys_checkpoint_dump(const char *file_format, ...);
 
 #define CHECKPOINT(name_format, ...) mys_checkpoint(name_format, ##__VA_ARGS__)
+
+/**
+ * @example
+
+int main()
+{
+    mys_checkpoint_reset();
+    CHECKPOINT("zzz-%06d", 1);
+    CHECKPOINT("zzz-%06d", 2);
+    CHECKPOINT("zzz-%06d", 3);
+    CHECKPOINT("zzz-%06d", 4);
+    int ret = mys_checkpoint_dump("checkpoint.%06d", MYRANK());
+    return 0;
+}
+
+ */
