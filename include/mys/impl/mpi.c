7369aa962ea4d4b9280438e7a7811128c006719e
Mayeths <wow@mayeths.com>
2023-11-28 10:14:23 +0800

    Use MPI_ instead of PMPI_

--- include/mys/impl/mpi.c
+++ include/mys/impl/mpi.c
@@ -81,62 +81,62 @@ MYS_API _mys_MPI_Comm mys_mpi_comm()
 
 MYS_STATIC int _mys_MPI_Initialized(int *flag)
 {
-    return PMPI_Initialized(flag);
+    return MPI_Initialized(flag);
 }
 
 MYS_STATIC int _mys_MPI_Init_thread(int *argc, char ***argv, int required, int *provided)
 {
-    return PMPI_Init_thread(argc, argv, required, provided);
+    return MPI_Init_thread(argc, argv, required, provided);
 }
 
 MYS_STATIC int _mys_MPI_Comm_rank(_mys_MPI_Comm comm, int *rank)
 {
-    return PMPI_Comm_rank(comm, rank);
+    return MPI_Comm_rank(comm, rank);
 }
 
 MYS_STATIC int _mys_MPI_Comm_size(_mys_MPI_Comm comm, int *size)
 {
-    return PMPI_Comm_size(comm, size);
+    return MPI_Comm_size(comm, size);
 }
 
 MYS_STATIC int _mys_MPI_Probe(int source, int tag, _mys_MPI_Comm comm, _mys_MPI_Status *status)
 {
-    return PMPI_Probe(source, tag, comm, status);
+    return MPI_Probe(source, tag, comm, status);
 }
 
 MYS_STATIC int _mys_MPI_Get_count(_mys_MPI_Status *status, _mys_MPI_Datatype datatype, int *count)
 {
-    return PMPI_Get_count(status, datatype, count);
+    return MPI_Get_count(status, datatype, count);
 }
 
 MYS_STATIC int _mys_MPI_Recv(void *buf, int count, _mys_MPI_Datatype datatype, int source, int tag, _mys_MPI_Comm comm, _mys_MPI_Status *status)
 {
-    return PMPI_Recv(buf, count, datatype, source, tag, comm, status);
+    return MPI_Recv(buf, count, datatype, source, tag, comm, status);
 }
 
 MYS_STATIC int _mys_MPI_Send(const void *buf, int count, _mys_MPI_Datatype datatype, int dest, int tag, _mys_MPI_Comm comm)
 {
-    return PMPI_Send(buf, count, datatype, dest, tag, comm);
+    return MPI_Send(buf, count, datatype, dest, tag, comm);
 }
 
 MYS_STATIC int _mys_MPI_Barrier(_mys_MPI_Comm comm)
 {
-    return PMPI_Barrier(comm);
+    return MPI_Barrier(comm);
 }
 
 MYS_STATIC int _mys_MPI_Allreduce(void *sendbuf, void *recvbuf, int count, _mys_MPI_Datatype datatype, _mys_MPI_Op op, _mys_MPI_Comm comm)
 {
-    return PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+    return MPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
 }
 
 MYS_STATIC int _mys_MPI_Bcast(void *buffer, int count, _mys_MPI_Datatype datatype, int root, _mys_MPI_Comm comm)
 {
-   return PMPI_Bcast(buffer, count, datatype, root, comm);
+   return MPI_Bcast(buffer, count, datatype, root, comm);
 }
 
 MYS_STATIC double _mys_MPI_Wtime()
 {
-    return PMPI_Wtime();
+    return MPI_Wtime();
 }
 
 
