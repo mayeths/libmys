702f27fdc6fa3fe93ce0b18fe91432619a372ad2
Mayeths <wow@mayeths.com>
2024-12-08 20:00:13 +0800

    Add mys_hrreset() and mys_hrsync()

--- include/mys/hrtime.h
+++ include/mys/hrtime.h
@@ -21,62 +21,41 @@
 #include "mpi.h"
 #include "thread.h"
 
-
+// The default high-resolution timer
 MYS_PUBLIC const char *mys_hrname();
 MYS_PUBLIC uint64_t mys_hrtick();
 MYS_PUBLIC uint64_t mys_hrfreq();
 MYS_PUBLIC double mys_hrtime();
-
-MYS_PUBLIC const char *mys_hrname_mpi();
-MYS_PUBLIC uint64_t mys_hrtick_mpi();
-MYS_PUBLIC uint64_t mys_hrfreq_mpi();
-MYS_PUBLIC double mys_hrtime_mpi();
-
-MYS_PUBLIC const char *mys_hrname_openmp();
-MYS_PUBLIC uint64_t mys_hrtick_openmp();
-MYS_PUBLIC uint64_t mys_hrfreq_openmp();
-MYS_PUBLIC double mys_hrtime_openmp();
+MYS_PUBLIC void mys_hrreset();
+MYS_PUBLIC void mys_hrsync();
 
 #if defined(ARCH_AARCH64)
-#define MYS_ENABLED_HRTIMER_AACH64
+#define MYS_HRTIMER_HAVE_AACH64
 MYS_PUBLIC const char *mys_hrname_aarch64();
 MYS_PUBLIC uint64_t mys_hrtick_aarch64();
 MYS_PUBLIC uint64_t mys_hrfreq_aarch64();
 MYS_PUBLIC double mys_hrtime_aarch64();
-#endif
-
-#if defined(ARCH_X64) && defined(TSC_FREQ) && TSC_FREQ > 1
-#define MYS_ENABLED_HRTIMER_X64
-MYS_PUBLIC const char *mys_hrname_x64();
-MYS_PUBLIC uint64_t mys_hrtick_x64();
-MYS_PUBLIC uint64_t mys_hrfreq_x64();
-MYS_PUBLIC double mys_hrtime_x64();
+MYS_PUBLIC void mys_hrreset_aarch64();
+MYS_PUBLIC void mys_hrsync_aarch64();
 #endif
 
 #if defined(POSIX_COMPLIANCE)
-#define MYS_ENABLED_HRTIMER_POSIX
+#define MYS_HRTIMER_HAVE_POSIX
 MYS_PUBLIC const char *mys_hrname_posix();
 MYS_PUBLIC uint64_t mys_hrtick_posix();
 MYS_PUBLIC uint64_t mys_hrfreq_posix();
 MYS_PUBLIC double mys_hrtime_posix();
+MYS_PUBLIC void mys_hrreset_posix();
+MYS_PUBLIC void mys_hrsync_posix();
 #endif
 
-#if defined(__cplusplus)
-#define MYS_ENABLED_HRTIMER_CXX
-MYS_PUBLIC const char *mys_hrname_cxx();
-MYS_PUBLIC uint64_t mys_hrtick_cxx();
-MYS_PUBLIC uint64_t mys_hrfreq_cxx();
-MYS_PUBLIC double mys_hrtime_cxx();
-#endif
-
-#if defined(OS_WINDOWS)
-#define MYS_ENABLED_HRTIMER_WINDOWS
-MYS_PUBLIC const char *mys_hrname_windows();
-MYS_PUBLIC uint64_t mys_hrtick_windows();
-MYS_PUBLIC uint64_t mys_hrfreq_windows();
-MYS_PUBLIC double mys_hrtime_windows();
-#endif
-
+#define MYS_HRTIMER_HAVE_MPI
+MYS_PUBLIC const char *mys_hrname_mpi();
+MYS_PUBLIC uint64_t mys_hrtick_mpi();
+MYS_PUBLIC uint64_t mys_hrfreq_mpi();
+MYS_PUBLIC double mys_hrtime_mpi();
+MYS_PUBLIC void mys_hrreset_mpi();
+MYS_PUBLIC void mys_hrsync_mpi();
 
 /* g++ -std=c++11 -I../include -lm ./a.cpp && ./a.out
 #include <stdlib.h>
