01a66d731ada8e0026aceaff591eca265ed0c218
Mayeths <wow@mayeths.com>
2025-03-21 10:10:14 +0800

    Free group by mys_free2

--- include/mys/impl/commgroup.c
+++ include/mys/impl/commgroup.c
@@ -165,7 +165,7 @@ MYS_PUBLIC void mys_commgroup_release(mys_commgroup_t *group)
     mys_free2(MYS_ARENA_COMMGROUP, group->_group_ids, sizeof(int) * group->global_nranks);
     mys_free2(MYS_ARENA_COMMGROUP, group->_local_ranks, sizeof(int) * group->global_nranks);
     mys_free2(MYS_ARENA_COMMGROUP, group->_brothers, sizeof(int) * group->local_nranks);
-    free(group);
+    mys_free2(MYS_ARENA_COMMGROUP, group, sizeof(mys_commgroup_t));
 }
 
 MYS_PUBLIC mys_commgroup_t *mys_commgroup_create_node(mys_MPI_Comm global_comm)
