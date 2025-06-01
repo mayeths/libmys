5a6a635a55c5d642caae7b4c94f81baafd0d70d3
mayeths <wow@mayeths.com>
2025-06-01 10:16:33 +0800

    Add vimrc

--- etc/how-to-use.sh
+++ etc/how-to-use.sh
@@ -1,3 +1,8 @@
+
+# hammperspoon
 ln -s ${MYS_DIR}/etc/hammerspoon/init.lua ~/.hammerspoon/init.lua
 ln -s ${MYS_DIR}/etc/hammerspoon/modules/clipboard.lua ~/.hammerspoon/modules/clipboard.lua
 ln -s ${MYS_DIR}/etc/hammerspoon/modules/test.lua ~/.hammerspoon/modules/test.lua
+
+# vim
+ln -s ${MYS_DIR}/etc/vim/vimrc ~/.vimrc
